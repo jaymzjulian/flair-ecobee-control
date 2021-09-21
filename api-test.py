e56259106cf42cb5fc743f7e3af9f253fa98f689
Jaymz Julian <jaymz@artificial-stupidity.net>
2021-09-21 19:37:50 +1000

    fix repo

--- api-test.py
+++ api-test.py
@@ -1,10 +1,12 @@
 from flair_api import make_client
+import sys
 import shelve
+import math
 from datetime import datetime
 import pytz
 from six.moves import input
 from pyecobee import *
-
+import requests
 
 def persist_to_shelf(file_name, ecobee_service):
     pyecobee_db = shelve.open(file_name, protocol=2)
@@ -46,38 +48,140 @@ def authorize(ecobee_service):
 
 
 from secrets import *
+from settings import *
+
+
 
 client = make_client(client_id, client_secret, 'https://api.flair.co/')
 
 # retrieve a list of structures available to this account
 structures = client.get('structures')
+mode = structures[0].attributes['structure-heat-cool-mode']
+print("Mode is",mode)
 
+no_mode_room = ["Kman"]
 rooms = client.get('rooms')
 
 cooling = False
 heating = False
+parking = False
+delta = 0.0
+intake_temp = 999
+# If using an intake room, find that first since earlier rooms
+# may need it
+if use_intake_room:
+  for room in rooms:
+    if room.attributes['active'] == True:
+      if room.attributes['name'] == intake_room:
+        intake_temp = room.attributes['current-temperature-c']
+        print("Found intake room!",intake_temp)
+
+rcount = 0
+force_park = False
+max_delta = -999
+min_delta = 999
 for room in rooms:
+ if room.attributes['active'] == True:
   print(room.attributes['name'])
+  ctemp = room.attributes['current-temperature-c']
+  dtemp = room.attributes['set-point-c']
+
+  # Simplied C to F, since flair always repots in C
+  if switch_is_f:
+    n_delta = (ctemp - dtemp) * (9.0/5.0)
+  else:
+    n_delta = (ctemp - dtemp)
+
+  if max_delta < n_delta:
+    max_delta = n_delta
+  if min_delta > n_delta:
+    min_delta = n_delta
+
+  print(n_delta)
+  if not room.attributes['name'] in no_mode_room:
+    # if the user set delta_is_max, only update this if the abs is higher - this way we
+    # get the highest room in the system
+
+    # Either way, delta is awlays squared with sign added - this is a least squares situation
+    if delta_is_max:
+      new_delta = (n_delta*n_delta)*math.copysign(1, n_delta)
+      #print(delta,new_delta)
+      if math.fabs(new_delta) > math.fabs(delta):
+        delta = new_delta
+    else:
+      delta += (n_delta*n_delta)*math.copysign(1, n_delta)
+    rcount += 1
+
+  # Don't do intake room stuff on the intake room :)
+  if room.attributes['name'] == intake_room:
+    iamintake = True
+  else:
+    iamintake = False
+
   for vent in room.get_rel('vents'):
+    if vent.attributes['inactive']:
+      print("dead vebt :(")
+      force_park = True
+      #sys.exit(1)
     c = vent.attributes['percent-open-reason']
-    ctemp = room.attributes['current-temperature-c']
-    dtemp = room.attributes['set-point-c']
     if 'is cooling' in c:
-      if int(ctemp*10.0) > int(dtemp*10.0):
-        print("room is cooling")
-        cooling = True
+      if int(ctemp*10.0 - 5) > int(dtemp*10.0):
+        if (not iamintake) and ctemp > (intake_temp + 0.5) and use_intake_room and (dtemp - ctemp > -2.0):
+          print("Room is more than intake temp, give or take - parking instead and relying on fan")
+          parking = True
+        else:
+          print("room is cooling")
+          cooling = True
       else:
         print("Skipping cooling because ctemp is actually lower than target",ctemp,"vs",dtemp)
+        parking = True
     if 'is heating' in c:
-      if int(ctemp*10.0) > int(dtemp*10.0):
-        print("room is cooling")
-        heating = True
+      if int(ctemp*10.0 + 5) < int(dtemp*10.0):
+        if (not iamintake) and ctemp < (intake_temp - 0.5) and use_intake_room and (dtemp - ctemp < 2.0):
+          print("Room is less than intake temp - parking instead and relying on fan", ctemp, intake_temp, dtemp - ctemp)
+          parking = True
+        else:
+          print("room is heating")
+          heating = True
       else:
-        print("Skipping cooling because ctemp is actually higher than target",ctemp,"vs",dtemp)
+        print("Skipping heating because ctemp is actually higher than target",ctemp,"vs",dtemp)
+        parking = True
+
+if delta_is_average:
+  delta /= rcount
+if force_park:
+  delta = 0
+  parking = True
+  cooling = False
+  heating = False
 print("Cooling:",cooling)
 print("Heating:",heating)
+print("parking",parking)
+print("Delta Temp:",delta, min_delta, max_delta)
 
+# if we have to only move a small amount, tell the
+# thermostat that instead of the max delta specified
+min_delta *= 10
+max_delta *= 10
+if cool_offs > max_delta:
+  cool_offs = max_delta
+if heat_offs > 0-min_delta:
+  heat_offs = 0-min_delta
+#print(cool_offs, heat_offs)
 
+if (cooling == False and heating == False) or only_switch_when_complete == False:
+ if delta > cool_switch_threshold and mode == 'heat':
+  print("House is heating, but overall delta is",delta,"above target - switching to cooling")
+  structures[0].update(attributes={'structure-heat-cool-mode': 'cool'})
+  heating = False
+  coolibg = False
+  parking = True
+if delta < 0-heat_switch_threshold and mode == 'cool':
+  print("House is cooling, but overall delta is",0-delta,"below target - switching to heating")
+  structures[0].update(attributes={'structure-heat-cool-mode': 'heat'})
+  heating = False
+  coolibg = False
+  parking = True
 
 try:
   pyecobee_db = shelve.open('pyecobee_db', protocol=2)
@@ -104,21 +208,36 @@ ts = ecobee_service.request_thermostats(Selection(selection_type=SelectionType.R
 #print(ts.pretty_format())
 temp = ts.thermostat_list[0].runtime.actual_temperature
 if cooling:
-  max_desired = (temp - 20)
+  max_desired = (temp - cool_offs)
   desired = ts.thermostat_list[0].runtime.desired_cool
-  if desired > max_desired:
+  if desired !=  max_desired:
     print("Updating to",max_desired/10)
     update_thermostat_response = ecobee_service.set_hold(cool_hold_temp=(max_desired / 10), heat_hold_temp=(max_desired / 10) + 6, hold_type=HoldType.INDEFINITE)
   else:
     print("Cooling is okay!")
 elif heating:
-  max_desired = (temp + 20)
+  max_desired = (temp + heat_offs)
   desired = ts.thermostat_list[0].runtime.desired_heat
-  if desired < max_desired:
+  if desired != max_desired:
     print("Updating to",max_desired/10)
     update_thermostat_response = ecobee_service.set_hold(cool_hold_temp=(max_desired / 10)-4, heat_hold_temp=(max_desired / 10), hold_type=HoldType.INDEFINITE)
   else:
     print("Heating is okay!")
+elif parking:
+  if ts.thermostat_list[0].settings.hvac_mode=='cool':
+    desired = ts.thermostat_list[0].runtime.desired_cool
+    hvac_mode = 'cool'
+  else:
+    desired = ts.thermostat_list[0].runtime.desired_heat
+    hvac_mode = 'head'
+  max_desired = temp
+  print(temp,desired)
+  if desired != max_desired:
+    print("Updating to",max_desired/10)
+    update_thermostat_response = ecobee_service.set_hold(cool_hold_temp=(max_desired / 10), heat_hold_temp=(max_desired / 10), hold_type=HoldType.INDEFINITE)
+  else:
+    print("parking is okay!")
+
 else:
   # FIXME: select heat/cool based on actual temprature
   if ts.thermostat_list[0].settings.hvac_mode=='cool':
@@ -131,9 +250,9 @@ else:
   # Ensure we're just in range....
   if temp >= desired and hvac_mode == 'cool':
       print("Updatring to no cooling/heating")
-      update_thermostat_response = ecobee_service.set_hold(cool_hold_temp=(temp / 10)+2, heat_hold_temp=(temp / 10) - 2, hold_type=HoldType.INDEFINITE)
+      update_thermostat_response = ecobee_service.set_hold(cool_hold_temp=(temp / 10)+1, heat_hold_temp=(temp / 10) - 1, hold_type=HoldType.INDEFINITE)
   
   elif temp <= desired and hvac_mode == 'heat':
-      update_thermostat_response = ecobee_service.set_hold(cool_hold_temp=(temp / 10)+2, heat_hold_temp=(temp / 10) - 2, hold_type=HoldType.INDEFINITE)
+      update_thermostat_response = ecobee_service.set_hold(cool_hold_temp=(temp / 10)+1, heat_hold_temp=(temp / 10) - 1, hold_type=HoldType.INDEFINITE)
   else:
       print("House is okay!")
--- demo-secrets.py
+++ demo-secrets.py
@@ -1,4 +1,7 @@
+# To use this, copy this file to `secrets.py` and 
+# then fill in your details
 client_id = 'flair client id'
 client_secret = 'flair secret'
 ecobee_name = 'cat and duck inc'
-ecobee_api_key = 'ecobee api key'
+ecobee_api_key = 'ecobee api key
+'
